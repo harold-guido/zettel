4c7a7e96c2eb6fbb77213e1bbbc3f417073e0612
Harold Parra <gc.harold.parra@gmail.com>
2023-04-05 17:02:45 +0100

    Added partially functional update command

--- luhm.py
+++ luhm.py
@@ -1,19 +1,20 @@
 #!/usr/bin/enb python3
 
 import argparse
-from modules.system_init import init
+from modules.system_init import init, update
 
 parser = argparse.ArgumentParser(description="")
 subparser = parser.add_subparsers(dest="command")
 
 init_parser = subparser.add_parser("init", help="")
+init_parser = subparser.add_parser("update", help="")
 
 args = parser.parse_args()
 
 if args.command == "init":
     init()
 elif args.command == "update":
-    update_bibliographies()
+    update()
 elif args.command == "add":
     add_file(args.file)
 elif args.command == "stat":
--- modules/system_init.py
+++ modules/system_init.py
@@ -3,11 +3,11 @@ import yaml
 import re
 
 # declaring variables used throughout module
-registers = ['', '.notes-register', '.references-register', '.projects-register']
-metadata_pattern = re.compile(r'^---\n(.*?)\n---', re.DOTALL | re.MULTILINE)
-file_type = {1:"note", 2:"reference", 3:"project"}
+REGISTERS = ['', '.notes-register', '.references-register', '.projects-register']
+FILE_TYPES = {1:"note", 2:"reference", 3:"project"}
 
 def update_metadata(path: str, field: str, value: str) -> None:
+    metadata_pattern = re.compile(r'^---\n(.*?)\n---', re.DOTALL | re.MULTILINE)
     # update yaml metadata
     with open(path, 'r') as file:
         content = file.read()
@@ -46,12 +46,13 @@ def find_markdowns(directory):
     return root_markdowns
 
 def init():
+    # links = {name: {frontlinks:[]}, backlinks:[]}}
     # initialize zettelkasten in current directory
     current_directory = os.getcwd()
 
     markdowns = find_markdowns(current_directory)
 
-    for register in registers:
+    for register in REGISTERS:
         os.makedirs(os.path.join(current_directory, register), exist_ok=True)
         with open(os.path.join(current_directory, register, 'Bibliography.md'), 'w') as bibliography:
             bibliography.write(f"# Bibliography{register}\n")
@@ -70,34 +71,81 @@ def init():
                    print("Enter 1, 2, or 3")
                    choice = int(input("(1/2/3)"))
 
-                update_metadata(os.path.join(root, markdown), "type", file_type[choice])
+                update_metadata(os.path.join(root, markdown), "type", FILE_TYPES[choice])
             else:
                 update_metadata(os.path.join(root, markdown), "type", "note")
 
-    update()
+    update(markdowns)
 
-def update(current_directory=os.getcwd()):
+def get_links(path):
+    with open(path, 'r') as file:
+        content = file.read()
+
+    # match the path string inside of each link
+    link_pattern = re.compile(r'\[.*?\]\((.*?)\)', re.MULTILINE)
+    # get list of paths
+    links = link_pattern.findall(content)
+    # remove duplicates
+    links = list(dict.fromkeys(links))
+    file_links = []
+    broken_file_links = []
+
+    for path in links:
+        # check if link is to an existing file
+        if os.path.isfile(path):
+            file_links.append(path)
+        elif not os.path.isfile(path) and os.path.isdir(os.path.dirname(path)):
+            file_links.append(path)
+            broken_file_links.append(path)
+
+    return [file_links, broken_file_links]
+
+def draw_tree(links):
+    
+    return("Hello World")
+    # draws file tree diagram of link structure within each bibliography
+
+def update(markdowns={}, current_directory=os.getcwd()):
+    print('runs update')
+
     # update metadata: type, links, backlinks using yaml
 
     # create dictionary in format:
-    # links = {name: {links:[], backlinks[]}}
+    # links = {name: {links:[], backlinks:[]}}
+    links = {}
 
-    # it is at least the user's responsibility to use
-    # [link](./filename) or [[filename]] as formats in their links
+    # NOTE: It is at least the user's responsibility to use [link](./filename)
+    # NOTE: For now user has to write link paths relative to root directory of zettel
+    # NOTE: Relative paths in format
+    # NOTE: ./file or ./dir/dir/.../file
 
-    # first go through each markdown file and collect links in format []() or format [[]]
-    # update [[]] formatted links as []() formatted links
+    # first go through each markdown file and collect links in format []()
+    markdowns = find_markdowns(current_directory) if markdowns != find_markdowns(current_directory) else markdowns
 
-    # populate links[name] and links[name][links]
+    for root in markdowns:
+        # populate front links of links dict
+        for markdown in markdowns.get(root):
+            links.update({ os.path.join(root, markdown):{ 
+                # get front links in format [[link paths],[broken or todo links]]
+                'front_links': get_links(os.path.join(root, markdown)), 'back_links': []}})
+
+    # notify user of potentially broken or uncompleted links
+
+    # populate backlinks
+    # use os.path.samefile(path1, path2)
+
+    for markdown_path_reference in links:
+        for markdown_path_check in links:
+            if markdown_path_reference != markdown_path_check:
+                for file_path in links.get(markdown_path_check).get('front_links')[0]:
+                    if file_path not in links.get(markdown_path_check).get('front_links')[1]:
+                        if os.path.samefile(markdown_path_reference, file_path):
+                            links.get(markdown_path_reference).get('back_links').append(markdown_path_check)
+
+    # using os.path.basename(path)
+    draw_tree(links)
 
     # loop through links[name] and for each link add the corresponding backlink to file if it exists
     # if file does not exist add link name to a links["to-do"]
 
-    # create make_tree() function to create a tree based on links and backlinks
-    print("Hello")
-
-
-    
-
-
-            
+    # create draw_tree() function to create a tree based on links and backlinks
